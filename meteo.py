ca21f21b1b6f06adeafaab19a2ecc9cc303f87a2
Bitrey <alessandro.amella@live.it>
2023-12-14 12:00:05 +0100

    huh

---
+++ meteo.py
@@ -0,0 +1,28 @@
+import socketserver
+import datetime
+import requests
+
+
+class MyTCPSocketHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        res = requests.get(
+            "https://api.openweathermap.org/data/2.5/weather?q=Modena&appid=$$")
+
+        data = res.json()
+        temp = (float(data['main']['temp']) - 273.15).__round__(4)
+        desc = data['weather'][0]['description']
+
+        self.request.sendall(
+            f'Temperature: {temp}°C, description: {desc}'.encode())
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 2002
+
+    server = socketserver.TCPServer((HOST, PORT), MyTCPSocketHandler)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("server closing")
+        server.server_close()
